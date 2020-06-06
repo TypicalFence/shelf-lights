5fdf9b860ce3d915fe9dd70ea4fb3db22edd9b75
gaston <gastoon89@gmail.com>
2020-06-07 01:33:18 +0200

    led streifä

--- src/main.c
+++ src/main.c
@@ -1,15 +1,26 @@
 #include <owoLED.h>
 #include <util/delay.h>
+#include <avr/io.h> 
+
+#define PIXELS 60  // Number of pixels in the string
 
 int main (void) {
 
-    owoled_init();
-    while(1) {
-        owoled_toggle();
-        _delay_ms(500);
+    PIXEL_DDR = 0b00000001;
+    while (1) {
+
+        for (int i = 0; i<PIXELS/2; i++) {
+            svoid show()endPixel(0x00, 0x00, 0xff);
+        }
+
+        for (int i = 0; i<PIXELS/2; i++) {
+            sendPixel(0x77, 0x00, 0x00);
+        }
+
+        show();
+        _delay_ms(10);
     }
     
     return 0;
 }
-
 
