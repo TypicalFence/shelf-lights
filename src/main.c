84d24fce109140031bce4bbe0764cbf3a41d7df1
gaston <gastoon89@gmail.com>
2020-06-07 20:35:46 +0200

    adjust main.c

--- src/main.c
+++ src/main.c
@@ -5,16 +5,16 @@
 #define PIXELS 60  // Number of pixels in the string
 
 int main (void) {
+    OwOLedAddress addr = owoled_init(PORTB, DDRB, 0);
 
-    PIXEL_DDR = 0b00000001;
     while (1) {
 
         for (int i = 0; i<PIXELS/2; i++) {
-            svoid show()endPixel(0x00, 0x00, 0xff);
+            owoled_send_colors(&adress, 0x00, 0x00, 0xff);
         }
 
         for (int i = 0; i<PIXELS/2; i++) {
-            sendPixel(0x77, 0x00, 0x00);
+            owoled_send_colors(&adress, 0x77, 0x00, 0x00);
         }
 
         show();
