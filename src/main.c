d68261e726568190ca320b36de24a2a42032ce15
fence <fence@desu-mail.moe>
2020-06-09 04:13:42 +0200

    update owoled & fix code

--- src/main.c
+++ src/main.c
@@ -10,14 +10,14 @@ int main (void) {
     while (1) {
 
         for (int i = 0; i<PIXELS/2; i++) {
-            owoled_send_colors(&adress, 0x00, 0x00, 0xff);
+            owoled_send_colors(&addr, 0x00, 0x00, 0xff);
         }
 
         for (int i = 0; i<PIXELS/2; i++) {
-            owoled_send_colors(&adress, 0x77, 0x00, 0x00);
+            owoled_send_colors(&addr, 0x77, 0x00, 0x00);
         }
 
-        show();
+        owoled_show();
         _delay_ms(10);
     }
     
